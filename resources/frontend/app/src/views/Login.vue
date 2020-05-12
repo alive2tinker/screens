e1dfa884bb058e02f9ab8a7ef64822666cb862ad
alive2tinker <binsalbiz@icloud.com>
2020-05-12 14:04:26 +0300

    removed default login

--- resources/frontend/app/src/views/Login.vue
+++ resources/frontend/app/src/views/Login.vue
@@ -46,8 +46,8 @@
                 //eslint-disable-next-line
                 reg:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
                 form: {
-                    username: 'alsufiran@gmail.com',
-                    password: 'alive2tinker',
+                    username: '',
+                    password: '',
                 },
                 errors: [],
                 isLoading: false
